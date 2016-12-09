2cbaba882c98225de232b46d8b3d5e58808b16a5
perprit <jihwang@hcil.snu.ac.kr>
2016-12-09 15:32:09 +0900

    StatView separate tabs

--- stat-view.js
+++ stat-view.js
@@ -1,24 +1,68 @@
 var StatView = (function() {
+    // stopTags: authors tag that will be ignored
+    var stopTags = ["human computer interaction (hci)", "human-centered computing", "hci", "human factors"];
+    var tabObj = {"cooccur": {"isInit": false, "svg": {}},
+                "wordle": {"isInit": false, "svg": {}},
+                "ref": {"isInit": false, "svg": {}},
+                "year": {"isInit": false, "svg": {}}};
+    var currentTabName = "cooccur";
+    var selectedPapers = [];
+
     var view = d3.select("#stat-view-body");
     var margin = {top: 10, bottom: 10, left: 10, right: 10};
     var width = document.getElementById("stat-view-body").offsetWidth - margin.left - margin.right;
     var height = document.getElementById("stat-view-body").offsetHeight - margin.top - margin.bottom;
 
-    // cooccurSvg, wordleSvg, refSvg, yearSvg
-    var wordleSvg = view.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", "translate("+margin.left+","+margin.top+")");
+    function svgAppender(svgId) {
+        var svg =  view.append("svg")
+            .attr("id", svgId)
+            .attr("width", 0)
+            .attr("height", 0);
 
-    return {
-        update: function(selected) {
-            // wordle
+        svg.append("g")
+            .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+        return svg;
+    }
+
+    function selectTab(tabName) {
+        // adjusts tabs' svg width and height
+        // (to expose the selected one only)
+        tabObj[currentTabName].svg.attr("width", 0);
+        tabObj[currentTabName].svg.attr("height", 0);
+        tabObj[tabName].svg.attr("width", width + margin.left + margin.right);
+        tabObj[tabName].svg.attr("height", height + margin.top + margin.bottom);
+
+        // init svg contents if it is not inited
+        if(!tabObj[tabName].isInit) {
+            if (tabName === "cooccur") {
+                initCooccur(selectedPapers, tabObj["cooccur"].svg);
+            } else if (tabName === "wordle") {
+                initWordle(selectedPapers, tabObj["wordle"].svg);
+            } else if (tabName === "ref") {
+                initRef(selectedPapers, tabObj["ref"].svg);
+            } else if (tabName === "year") {
+                initYear(selectedPapers, tabObj["year"].svg);
+            } else {
+                console.log("unknown tabName: " + tabName);
+            }
+            tabObj[tabName].isInit = true;
+        }
+        currentTabName = tabName;
+
+        function initCooccur(selectedPapers, cooccurSvg) {
+            console.log("initCooccur");
+        }
+
+        function initWordle(selectedPapers, wordleSvg) {
             // calculate tag frequency list using wordles
             var tagObj = {}, tagList = [];
-            selected.forEach(function(d) {
+            selectedPapers.forEach(function(d) {
                 d.author_tags.forEach(function(tag) {
-                    if(!tagObj[tag]) {
+                    if(stopTags.includes(tag)) {
+                        return;
+                    }
+                    else if (!tagObj[tag]) {
                         tagObj[tag] = {tag: tag, papers: [d]};
                     } else {
                         tagObj[tag]["papers"].push(d);
@@ -29,15 +73,15 @@ var StatView = (function() {
                 tagList.push(tagObj[key]);
             });
             tagList.sort(function(a, b) { return b.papers.length - a.papers.length; });
-            tagList = tagList.slice(3, 33);
+            tagList = tagList.slice(0, 30);
 
             // create wordle layout
             var fill = d3.scale.category10();
-            var size = d3.scale.linear().domain(d3.extent(tagList, function(d) { return d.papers.length; })).range([6, 36]);
+            var size = d3.scale.log().domain(d3.extent(tagList, function(d) { return d.papers.length; })).range([6, 36]);
             var layout = d3.layout.cloud()
                 .size([width, height])
                 .words(tagList)
-                .padding(4)
+                .padding(3)
                 .rotate(function(d) { return 0; })
                 .text(function(d) { return d.tag; })
                 .font("Sans-serif")
@@ -45,7 +89,7 @@ var StatView = (function() {
                 .fontSize(function(d) { return size(d.papers.length); })
                 .on("end", function(data) {
                     fill.domain(Array.apply(null, {length: data.length}).map(Number.call, Number));
-                    var text = wordleSvg.attr("transform", "translate(" + [width/2, height/2] + ")")
+                    var text = wordleSvg.select("g").attr("transform", "translate(" + [width/2, height/2] + ")")
                         .selectAll("text")
                         .data(data, function(d) { return d.tag; });
 
@@ -77,6 +121,53 @@ var StatView = (function() {
                         .remove();
                 })
                 .start();
+        }
+
+        function initRef(selectedPapers, refSvg) {
+            console.log("initRef");
+        }
+
+        function initYear(selectedPapers, yearSvg) {
+            console.log("initYear");
+        }
+    }
+
+    return {
+        init: function() {
+            // append svg for each tabs
+            Object.keys(tabObj).forEach(function(tabName) {
+                tabObj[tabName].svg = svgAppender(tabName+"-svg");
+            });
+
+            // select default tab
+            selectTab(currentTabName);
+
+            // initalize tab click handler
+            // cooccur, wordle, ref, and year
+            $("#stat-view-tabs #tab-cooccur").click(function(e) {
+                e.preventDefault();
+                $(this).tab("show");
+                selectTab("cooccur");
+            });
+            $("#stat-view-tabs #tab-wordle").click(function(e) {
+                e.preventDefault();
+                $(this).tab("show");
+                selectTab("wordle");
+            });
+            $("#stat-view-tabs #tab-ref").click(function(e) {
+                e.preventDefault();
+                $(this).tab("show");
+                selectTab("ref");
+            });
+            $("#stat-view-tabs #tab-year").click(function(e) {
+                e.preventDefault();
+                $(this).tab("show");
+                selectTab("year");
+            });
+        },
+        update: function(newPapers) {
+            selectedPapers = newPapers;
+            selectTab(currentTabName);
         }
     };
 })();
