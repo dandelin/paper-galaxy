bf337a0c4ff49838b99bb1070f7d7b973bc6fe75
perprit <jihwang@hcil.snu.ac.kr>
2016-12-12 18:51:11 +0900

    DetailView hotfix

--- detail-view.js
+++ detail-view.js
@@ -66,6 +66,7 @@ var DetailView = (function() {
                     controller.updateSelectedCircles(d3.selectAll(".paper")
                         .filter(function(paper) { return paper.author_tags.includes(word) && paperIds.includes(paper.id); }));
                 });
+            selectors.abstract.html(newPaper.abstract);
         },
     };
 })();
